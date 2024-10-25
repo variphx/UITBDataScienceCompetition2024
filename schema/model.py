e48e8f4b973febe67a2ed8bfc7a8ff2771bb624d
variphx <variphx@gmail.com>
2024-10-25 19:33:43 +0000

    fix model

--- schema/model.py
+++ schema/model.py
@@ -57,14 +57,14 @@ class VimmsdModel(_nn.Module):
     def forward(self, image, text):
         image_outputs = self._image_model(image)
         image_outputs = image_outputs.reshape([image_outputs.shape[0], -1])
-        image_outputs = _torch.tensor(image_outputs, device=self._device)
+        image_outputs = _torch.as_tensor(image_outputs, device=self._device)
         print(image_outputs)
 
         text_outputs = self._text_model(text)
         text_outputs = text_outputs.reshape([text_outputs.shape[0], -1])
-        text_outputs = _torch.tensor(text_outputs, device=self._device)
+        text_outputs = _torch.as_tensor(text_outputs, device=self._device)
         print(text_outputs)
 
-        combined = _torch.cat([image_outputs, text_outputs], dim=-1)
+        combined = _torch.cat([image_outputs, text_outputs], dim=1)
         logits = self._fc(combined)
         return logits
