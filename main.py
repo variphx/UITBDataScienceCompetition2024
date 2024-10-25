d484f599d27165370e0b54a26aa7e9dd50746aaa
variphx <variphx@gmail.com>
2024-10-25 19:58:58 +0000

    fix model

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import json
 from schema import dataset, trainer, model
 import torch
+from torch import nn
+from torch.optim import AdamW, lr_scheduler
 from torch.utils.data import DataLoader
 from torch.nn import functional as F
 from torchvision.transforms.v2 import Compose, ToImage, ToDtype
 from transformers import TrainingArguments
+from torchmetrics.classification import MulticlassF1Score
+from tqdm import tqdm
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -32,26 +36,51 @@ vimmsd_model(**(next(iter(decoy_dataloader))["features"]))
 
 del decoy_dataloader
 
-training_args = TrainingArguments(
-    output_dir="/kaggle/working/model",
-    overwrite_output_dir=True,
-    per_device_train_batch_size=64,
-    num_train_epochs=15,
-    log_level="debug",
-    logging_strategy="steps",
-    logging_steps=1,
-    save_strategy="no",
-    report_to="none",
-)
+# training_args = TrainingArguments(
+#     output_dir="/kaggle/working/model",
+#     overwrite_output_dir=True,
+#     per_device_train_batch_size=64,
+#     num_train_epochs=15,
+#     log_level="debug",
+#     logging_strategy="steps",
+#     logging_steps=1,
+#     save_strategy="no",
+#     report_to="none",
+# )
 
-model_trainer = trainer.Trainer(
-    model=vimmsd_model,
-    args=training_args,
-    train_dataset=train_dataset,
-)
+# model_trainer = trainer.Trainer(
+#     model=vimmsd_model,
+#     args=training_args,
+#     train_dataset=train_dataset,
+# )
 
-model_trainer.train()
-model_trainer.save_model()
+# model_trainer.train()
+# model_trainer.save_model()
+train_dataloader = DataLoader(
+    train_dataset, batch_size=64, collate_fn=trainer.train_collate_fn
+)
+epochs = 30
+optimizer = AdamW(vimmsd_model.parameters(), 1e-4)
+scheduler = lr_scheduler.CosineAnnealingLR(
+    optimizer, T_max=epochs * len(train_dataloader)
+)
+f1_metric = MulticlassF1Score(num_classes=4).to(device=device)
+for epoch in range(epochs):
+    for batch in (
+        progress_bar := tqdm(train_dataloader, desc=f"epoch={epoch}/{epochs}")
+    ):
+        optimizer.zero_grad()
+        features, targets = batch["features"], batch["target"]
+        targets = torch.as_tensor(targets, device=device)
+        logits = model(features)
+        loss = F.cross_entropy(logits, targets)
+        loss.backward()
+        nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+        optimizer.step()
+        scheduler.step()
+        f1_score = f1_metric(F.softmax(logits, dim=1), targets)
+        progress_bar.set_postfix({"loss": f"{loss.item():.4f}", "f1": f"{f1_score}"})
+
 
 infer_dataset = dataset.VimmsdDataset(
     data_file="/kaggle/input/vimmsd-uit2024/vimmsd-public-test.json",
@@ -62,7 +91,9 @@ infer_dataset = dataset.VimmsdDataset(
     image_processor=image_processor,
     task="infer",
 )
-infer_dataloader = DataLoader(infer_dataset, batch_size=1)
+infer_dataloader = DataLoader(
+    infer_dataset, batch_size=1, collate_fn=trainer.infer_collate_fn
+)
 
 results = {}
 for id, batch in enumerate(infer_dataloader):
--- schema/trainer.py
+++ schema/trainer.py
@@ -9,6 +9,7 @@ image_processor = AutoImageProcessor.from_pretrained(
 )
 
 
+
 def train_collate_fn(batch):
     features = {
         "image": image_processor(
@@ -22,29 +23,12 @@ def train_collate_fn(batch):
     return {"features": features, "target": targets}
 
 
-from transformers import Trainer as _Trainer
-from torch.nn import functional as _F
-from torch import optim as _optim
-from torch.optim import lr_scheduler as _lr_scheduler
-
-
-class Trainer(_Trainer):
-    def compute_loss(self, model, inputs, return_outputs=False):
-        features, targets = inputs["features"], inputs["target"]
-        logits = model(**features)
-        predictions = _F.softmax(logits, dim=1)
-        loss = _F.cross_entropy(predictions, targets)
-        return (loss, logits) if return_outputs else loss
-
-    def create_optimizer(self):
-        return _optim.AdamW(self.model.parameters())
-
-    def create_scheduler(self, num_training_steps, optimizer=None):
-        return _lr_scheduler.CosineAnnealingLR(
-            optimizer=optimizer, T_max=num_training_steps
-        )
-
-    def create_optimizer_and_scheduler(self, num_training_steps):
-        optimizer = self.create_optimizer()
-        scheduler = self.create_scheduler(num_training_steps, optimizer)
-        return (optimizer, scheduler)
+def infer_collate_fn(batch):
+    features = {
+        "image": image_processor(
+            images=[image_transforms(item["features"]["image"]) for item in batch],
+            return_tensors="pt",
+        ),
+        "text": [item["features"]["text"] for item in batch],
+    }
+    return {"features": features}
