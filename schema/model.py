b9b496a56ae06a19061c845a01470b0a5dba6d08
variphx <variphx@gmail.com>
2024-10-25 18:52:50 +0000

    fix model

--- schema/model.py
+++ schema/model.py
@@ -31,6 +31,7 @@ class ImageModel(_nn.Module):
         self._model = _get_peft_model(base_model, lora_config).to(self._device)
 
     def forward(self, x):
+        print(x)
         outputs = self._model(**x)
         print(outputs)
         return outputs
