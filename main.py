ab3b55a3d38c9fa898d1d65e15f216b66102bc23
variphx <variphx@gmail.com>
2024-10-26 02:09:15 +0000

    fix:

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from torch.optim import AdamW, lr_scheduler
 from torch.utils.data import DataLoader
 from torch.nn import functional as F
 from torchvision.transforms.v2 import Compose, ToImage, ToDtype
-from transformers import TrainingArguments
 from torchmetrics.classification import MulticlassF1Score
 from tqdm import tqdm
 
@@ -24,41 +23,12 @@ train_dataset = dataset.VimmsdDataset(
     class_names=class_names,
     task="train",
 )
-decoy_dataloader = DataLoader(
-    train_dataset,
-    batch_size=1,
-    collate_fn=trainer.train_collate_fn,
-)
-
-
-vimmsd_model = model.VimmsdModel(device=device).to(device)
-vimmsd_model(**(next(iter(decoy_dataloader))["features"]))
-
-del decoy_dataloader
-
-# training_args = TrainingArguments(
-#     output_dir="/kaggle/working/model",
-#     overwrite_output_dir=True,
-#     per_device_train_batch_size=64,
-#     num_train_epochs=15,
-#     log_level="debug",
-#     logging_strategy="steps",
-#     logging_steps=1,
-#     save_strategy="no",
-#     report_to="none",
-# )
-
-# model_trainer = trainer.Trainer(
-#     model=vimmsd_model,
-#     args=training_args,
-#     train_dataset=train_dataset,
-# )
-
-# model_trainer.train()
-# model_trainer.save_model()
 train_dataloader = DataLoader(
     train_dataset, batch_size=64, collate_fn=trainer.train_collate_fn
 )
+vimmsd_model = model.VimmsdModel(device=device).to(device)
+vimmsd_model(**(next(iter(train_dataloader))["features"]))
+
 epochs = 30
 optimizer = AdamW(vimmsd_model.parameters(), 1e-4)
 scheduler = lr_scheduler.CosineAnnealingLR(
@@ -87,9 +57,6 @@ infer_dataset = dataset.VimmsdDataset(
     data_file="/kaggle/input/vimmsd-uit2024/vimmsd-public-test.json",
     images_dir="/kaggle/input/vimmsd-uit2024/public-test-images/dev-images",
     class_names=class_names,
-    tokenizer=text_tokenizer,
-    image_transforms=image_transforms,
-    image_processor=image_processor,
     task="infer",
 )
 infer_dataloader = DataLoader(
@@ -97,12 +64,12 @@ infer_dataloader = DataLoader(
 )
 
 results = {}
-for id, batch in enumerate(infer_dataloader):
+for id, batch in (progress_bar := tqdm(enumerate(infer_dataloader), desc="infer")):
     features = batch["features"]
     logits = vimmsd_model(**features)
     predictions = F.softmax(logits, dim=1).argmax(dim=1)
     label = class_names[predictions[0]]
-    print(f"id={id} label={label}")
+    progress_bar.set_postfix({f"id={id}", f"label={label}"})
     results[str(id)] = label
 
 results = {
